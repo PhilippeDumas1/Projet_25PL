993486ea1ecf754497593c3f5f041fe43d26297b
Philippe DUMAS <philippe.dumas@student.junia.com>
2024-11-20 14:52:42 +0100

    5

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "Feucircu.hpp"
+#include "Voiture.hpp"
 
 
 int main()
@@ -11,11 +12,9 @@ int main()
     std::stop_source stopping;
     Traffic_light traffic_light_master{ Traffic_color::red };
     Traffic_light traffic_light_slave{ Traffic_color::red };
-    std::jthread thread_traffic_light_master(run_traffic_light,
-        std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
+    std::jthread thread_traffic_light_master(run_traffic_light, std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
 
-    std::jthread write_traffic_light(print_traffic_light,
-        std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
+    std::jthread write_traffic_light(print_traffic_light, std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "My window");
 
@@ -24,15 +23,28 @@ int main()
     sf::Vertex line2[] = { sf::Vertex(sf::Vector2f(0, l2)), sf::Vertex(sf::Vector2f(size, l2)) };
     sf::Vertex line3[] = { sf::Vertex(sf::Vector2f(l1, 0)), sf::Vertex(sf::Vector2f(l1, size)) };
     sf::Vertex line4[] = { sf::Vertex(sf::Vector2f(l2, 0)), sf::Vertex(sf::Vector2f(l2, size)) };
+ 
 
     sf::CircleShape circle1(radius);
     circle1.setFillColor(sf::Color::Blue);
     circle1.setOrigin(circle1.getRadius() / 2, circle1.getRadius() / 2);
     circle1.setPosition(l2 + radius / 2, l2 + radius / 2);
     sf::CircleShape circle2(radius);
+
     circle2.setFillColor(sf::Color::Green);
     circle2.setOrigin(circle2.getRadius() / 2, circle2.getRadius() / 2);
     circle2.setPosition(l2 + radius / 2, l1 - radius);
+
+	sf::CircleShape circle3(radius);
+	circle3.setFillColor(sf::Color::Blue);
+	circle3.setOrigin(circle3.getRadius() / 2, circle3.getRadius() / 2);
+	circle3.setPosition(l1 - radius, l2 - radius / 2);
+
+	sf::CircleShape circle4(radius);
+	circle4.setFillColor(sf::Color::Green);
+	circle4.setOrigin(circle4.getRadius() / 2, circle4.getRadius() / 2);
+	circle4.setPosition(l1 - radius / 2, l1 - radius / 2);
+
 
     while (window.isOpen())
     {
@@ -52,10 +64,16 @@ int main()
         window.draw(line2, 2, sf::Lines);
         window.draw(line3, 2, sf::Lines);
         window.draw(line4, 2, sf::Lines);
+
         circle1.setFillColor(get_SFML_color(traffic_light_slave));
         circle2.setFillColor(get_SFML_color(traffic_light_master));
+		circle3.setFillColor(get_SFML_color(traffic_light_master));
+		circle4.setFillColor(get_SFML_color(traffic_light_slave));
+
         window.draw(circle1);
         window.draw(circle2);
+		window.draw(circle3);
+		window.draw(circle4);
 
         window.display();
     }
